b46040ee571cb40bb1464a3c69a58c7da94eda40
robertfitstep <robertfitstep@gmail.com>
2017-12-01 11:44:21 +0100

    Add project files.

---
+++ DniVTyzdni/DniVTyzdni.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VypisDniVTyzdni
+{
+    static class DniVTyzdni
+    {
+        static string errMsg = "Zadali ste nesprávny deň v týždni";
+
+        static Dictionary<int, string> prvyPondelok = new Dictionary<int, string>
+        {
+            { 1,"Pondelok" },
+            { 2, "Utorok" },
+            { 3, "Streda" },
+            { 4, "Štvrtok" },
+            { 5, "Piatok" },
+            { 6, "Sobota" },
+            { 7, "Nedela" }
+        };
+
+        static Dictionary<int, string> prvaNedela = new Dictionary<int, string>
+        {
+            { 1, "Nedela" },
+            { 2,"Pondelok" },
+            { 3, "Utorok" },
+            { 4, "Streda" },
+            { 5, "Štvrtok" },
+            { 6, "Piatok" },
+            { 7, "Sobota" }
+        };
+
+        public static string VypisDenVTyzdni(string denVTyzdni)
+        {
+            return VypisDenVTyzdni(denVTyzdni, false);
+        }
+
+        public static string VypisDenVTyzdni(string denVTyzdni, bool nedelaPrva)
+        {
+            if (ValidujDen(denVTyzdni))
+            {
+                if (nedelaPrva)
+                {
+                    return prvaNedela[int.Parse(denVTyzdni)];
+                }
+                else if (!nedelaPrva)
+                {
+                    return prvyPondelok[int.Parse(denVTyzdni)];
+                }
+                else
+                {
+                    return errMsg;
+                }
+            }
+            else
+            {
+                return errMsg;
+            }
+        
+
+        bool ValidujDen(string den) {
+            int d = int.Parse(den);
+            if (d > 0 && d <= 7) { return true; }
+            else return false;            
+            }
+        }
+    }
+    }
+
+
---
+++ DniVTyzdni/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VypisDniVTyzdni
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deň 1 = {0}",DniVTyzdni.VypisDenVTyzdni("1"));
+            Console.WriteLine("Deň 2 = {0}", DniVTyzdni.VypisDenVTyzdni("2"));
+            Console.WriteLine("Deň 3 = {0}", DniVTyzdni.VypisDenVTyzdni("3"));
+            Console.WriteLine("Deň 4 = {0}", DniVTyzdni.VypisDenVTyzdni("4"));
+            Console.WriteLine("Deň 5 = {0}", DniVTyzdni.VypisDenVTyzdni("5"));
+            Console.WriteLine("Deň 6 = {0}", DniVTyzdni.VypisDenVTyzdni("6"));
+            Console.WriteLine("Deň 7 = {0}", DniVTyzdni.VypisDenVTyzdni("7"));
+            Console.WriteLine("\n");
+            Console.WriteLine("Deň 1 = {0} (Nedela prvá = true)", DniVTyzdni.VypisDenVTyzdni("1",true));
+            Console.WriteLine("Deň 2 = {0}", DniVTyzdni.VypisDenVTyzdni("2", true));
+            Console.WriteLine("Deň 3 = {0}", DniVTyzdni.VypisDenVTyzdni("3", true));
+            Console.WriteLine("Deň 4 = {0}", DniVTyzdni.VypisDenVTyzdni("4", true));
+            Console.WriteLine("Deň 5 = {0}", DniVTyzdni.VypisDenVTyzdni("5", true));
+            Console.WriteLine("Deň 6 = {0}", DniVTyzdni.VypisDenVTyzdni("6", true));
+            Console.WriteLine("Deň 7 = {0}", DniVTyzdni.VypisDenVTyzdni("7", true));
+            Console.ReadLine();
+            
+        }
+            
+
+    }
+}
